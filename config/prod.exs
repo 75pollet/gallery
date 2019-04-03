77ba4b1c1fcb43180c75882123805351d4a35892
7_5P <76pollet@gmail.com>
2019-04-03 17:52:16 +0300

    configure app for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :mygallery, MygalleryWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "art-gallery75", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -66,4 +68,9 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+
+config :mygallery, Mygallery.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+# import_config "prod.secret.exs"
