424795a3feea861f1eecdaf8d168654e8179a9ed
7_5P <76pollet@gmail.com>
2019-04-03 18:02:42 +0300

    remove comma

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :mygallery, MygalleryWeb.Endpoint,
   url: [scheme: "https", host: "art-gallery75", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
