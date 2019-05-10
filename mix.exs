2dde13db5389105125fd7741666a3867a7d66ace
7_5P <76pollet@gmail.com>
2019-05-10 18:38:13 +0300

    fix tests to make CI pass

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Mygallery.MixProject do
       {:phoenix, github: "phoenixframework/phoenix", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_integration, "~> 0.6", only: :test},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
